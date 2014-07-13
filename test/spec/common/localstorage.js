7df9c2ec8e809dc354b1fe5ba63475196f9e3b2c
zourtney <zourtney@gmail.com>
2014-07-13 01:04:26 -0500

    Missed one for wacky "_dependency_" syntax

--- test/spec/common/localstorage.js
+++ test/spec/common/localstorage.js
@@ -1,15 +1,15 @@
 'use strict';
 
-describe('Factor: localstorage', function() {
+describe('Factory: localstorage', function() {
 
   beforeEach(module('fauxcart.common'));
 
   //
   // Setup
   //
-  var ls;
-  beforeEach(inject(function(localstorage) {
-    ls = localstorage;
+  var localStorage;
+  beforeEach(inject(function(_localstorage_) {
+    localStorage = _localstorage_;
   }));
 
 
@@ -19,27 +19,27 @@ describe('Factor: localstorage', function() {
 
   // Simple strings
   it('should be able to set a string to window.localStorage', function() {
-    ls.set('TEST_STRING', 'this is the test string');
+    localStorage.set('TEST_STRING', 'this is the test string');
     expect(window.localStorage.TEST_STRING).toBe('this is the test string');
   });
 
   it('should be able to read strings from window.localStorage', function() {
-    ls.set('TEST_STRING', 'string value');
-    expect(ls.get('TEST_STRING')).toBe('string value');
+    localStorage.set('TEST_STRING', 'string value');
+    expect(localStorage.get('TEST_STRING')).toBe('string value');
   });
 
 
   // Complex objects
   it('should be able to serialize objects to window.localStorage', function() {
-    ls.set('TEST_OBJECT', {text: 'this is an object in local storage', num: 50});
+    localStorage.set('TEST_OBJECT', {text: 'this is an object in local storage', num: 50});
     var val = window.localStorage.TEST_OBJECT;
     expect(val).toContain('this is an object in local storage');
     expect(val).toContain('50');
   });
 
   it('should be able to read serialized objects from window.localStorage', function() {
-    ls.set('TEST_OBJECT', {name: 'Matt Dillon', year: 1876});
-    var val = ls.get('TEST_OBJECT');
+    localStorage.set('TEST_OBJECT', {name: 'Matt Dillon', year: 1876});
+    var val = localStorage.get('TEST_OBJECT');
     expect(val.name).toBe('Matt Dillon');
     expect(val.year).toBe(1876);
   });
