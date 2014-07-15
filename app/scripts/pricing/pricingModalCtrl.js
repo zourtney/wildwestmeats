5a06d90d697a19588e4aa25f475f02d8da8d093a
zourtney <zourtney@gmail.com>
2014-07-14 23:04:50 -0500

    Catch-all pricing modal controller note
    Because it's otherwise a little weird to share controllers amongst different screens.

--- app/scripts/pricing/pricingModalCtrl.js
+++ app/scripts/pricing/pricingModalCtrl.js
@@ -2,6 +2,9 @@
 
 angular.module('fauxcart.pricing')
 
+//NOTE: this is a catch-all controller for pricing-related modals since there
+//      is no real logic needed on these screens (yet);
+
 .controller('PricingModalCtrl', ['$scope', 'rule', function($scope, rule) {
 
   $scope.rule = rule;
