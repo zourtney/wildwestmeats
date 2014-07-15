c3970803c36ea583c796d49b751bb2f9b77c444c
zourtney <zourtney@gmail.com>
2014-07-14 21:01:08 -0500

    Styling!

--- app/scripts/products/productModalCtrl.js
+++ app/scripts/products/productModalCtrl.js
@@ -2,7 +2,7 @@
 
 angular.module('fauxcart.products')
 
-.controller('ProductEditCtrl', ['$scope', 'product', 'pricingRules', function($scope, product, pricingRules) {
+.controller('ProductModalCtrl', ['$scope', 'product', 'pricingRules', function($scope, product, pricingRules) {
 
   $scope.product = product;
   $scope.pricingRules = pricingRules;
